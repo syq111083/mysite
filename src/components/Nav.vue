0bfc58d0a2dd0ac072f6ba3bea0f7d30f3e17183
unknown <syq111083@hotmail.com>
2020-02-13 21:01:25 +0800

    '3'

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <div id="nav">
             <b-navbar type="dark" variant="dark" class="fixed-top">
-                <b-navbar-nav>
+                <b-navbar-nav class="text-center">
                     <b-nav-item>
                         <router-link to="/">首页</router-link>
                     </b-nav-item>
@@ -13,12 +13,15 @@
                         <router-link to="/register" >注册</router-link>
                     </b-nav-item>
                     <b-nav-item><router-link to="/comic">漫画</router-link></b-nav-item>
-                    <b-nav-item v-if="isLogin"><p class="text-success">欢迎, {{ nickname }}</p></b-nav-item>
-                    <b-nav-item v-if="isLogin"><b-link v-on:click="exit">退出登录</b-link></b-nav-item>
+                    <b-nav-item v-if="isLogin">欢迎, {{ nickname }}</b-nav-item>
+                    <b-nav-item v-if="isLogin"><b-button v-b-modal.modal-1>退出登录</b-button></b-nav-item>
                     <b-nav-item><router-link to="/email">关于</router-link></b-nav-item>
                 </b-navbar-nav>
             </b-navbar>
         </div>
+          <b-modal id="modal-1" title="退出" ok-title="退出" cancel-title="取消" @ok="exit()">
+            <p class="my-4">确定要退出吗？</p>
+          </b-modal>
         <router-view></router-view>
 
     </div>
